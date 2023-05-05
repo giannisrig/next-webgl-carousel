cc76859b29f55b4a25fcbf57a89a1e1f094dfa74
giannisrig <giannisrig93@gmail.com>
2023-05-05 10:55:30 +0300

    header changes

--- src/components/common/header/Logo.tsx
+++ src/components/common/header/Logo.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Logo() {
   return (
-    <Link href={"/"} className="flex items-center gap-[3px] transition-colors duration-200 hover:text-pink">
-      <Image src={"/images/demo/vercel.svg"} alt={"Logo of the Project"} width={25} height={25} />
-      <span className="text-2xl font-light text-amethyst">/</span>
-      <h4 className={`font-primary text-lg font-semibold`}>Starter App</h4>
+    <Link href={"/"} className="flex items-center gap-[4px] transition-colors duration-200 hover:text-pink">
+      <Image src={"/images/demo/vercel.svg"} alt={"Logo of the Project"} width={22} height={22} />
+      <span className="text-2xl font-light text-silver ">/</span>
+      <h4 className={`font-primary text-lg font-semibold`}>Next WebGL Carousel</h4>
     </Link>
   );
 }
