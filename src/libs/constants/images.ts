5bc458b6291febaddf1f9bbc811be0969dc4d6a1
giannisrig <giannisrig93@gmail.com>
2023-05-05 10:56:37 +0300

    Images data

---
+++ src/libs/constants/images.ts
@@ -0,0 +1,39 @@
+const imagesPath = "/images/carousel/";
+
+const images = [
+  {
+    image: imagesPath + "13.jpg",
+  },
+  {
+    image: imagesPath + "14.jpg",
+  },
+  {
+    image: imagesPath + "15.jpg",
+  },
+  {
+    image: imagesPath + "16.jpg",
+  },
+  {
+    image: imagesPath + "18.jpg",
+  },
+  {
+    image: imagesPath + "19.jpg",
+  },
+  {
+    image: imagesPath + "20.jpg",
+  },
+  {
+    image: imagesPath + "21.jpg",
+  },
+  {
+    image: imagesPath + "22.jpg",
+  },
+  {
+    image: imagesPath + "25.jpg",
+  },
+  {
+    image: imagesPath + "24.jpg",
+  },
+];
+
+export default images;
